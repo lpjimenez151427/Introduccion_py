c427113020dacf34f9afb7206e66b7bf90aaa8af
unknown <Lpjimenez151427@gmail.com>
2021-06-09 20:57:22 -0500

    operaciones basicas

---
+++ Funciones.py
@@ -0,0 +1,26 @@
+def sum(a,b):
+    resultado = a+b
+
+    return resultado
+
+def res(a,b):
+    resultado =a-b
+    return resultado 
+
+def mul(a,b):
+    resultado =a*b
+    return resultado
+    
+def div(a,b):
+    resultado= a/b
+    return resultado 
+
+
+print (sum(4,5))
+print (sum(4,12))
+print (res(5,8))
+print (res(14,5))
+print (mul(52,2))
+print (mul(73,4))
+print (div(52,2))
+print (div(85,4))
--- Listas.py
+++ Listas.py
@@ -5,7 +5,7 @@ print(Milista[2])
 Milista[3]="Laura"
 print(Milista)
 Primos=[1,3,5,7,11,13,17]
-#print(Primos[2]>=2)
+#print(Primos[2]>=2)ss
 print(Primos[0]==Primos[0])
 print(Primos[0]==Primos[2])
 print(Primos[1]!=Primos[0])
