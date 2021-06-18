1521326c6234897cfc268723b95d3354649d3f5f
unknown <Lpjimenez151427@gmail.com>
2021-06-17 20:09:50 -0500

    Funciones area

---
+++ Funciones_varias.py
@@ -0,0 +1,10 @@
+# circulo 
+PI = 3.14
+r = float(input("Introdusca el radio del circulo:"))
+Area = PI * r * r
+print("El area del circulo es = %.2f" %Area)
+
+#Cuadrado 
+def Area_of_Triangle(lado):
+    Area =  4 * lado  * lado 
+    print("\n El area del cuadrado es = %.2f" %Area)
