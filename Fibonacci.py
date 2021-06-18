feead3f4c0e46741abd252555b7f64e4176ed94a
unknown <Lpjimenez151427@gmail.com>
2021-06-17 19:00:10 -0500

    Secuencia de Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+Ndeserie  = int(input("numero de terminos? "))
+
+n1, n2 = 0, 1
+count = 0
+
+if Ndeserie <= 0:
+   print("Por favor ingrese el valor requerido")
+elif Ndeserie == 1:
+   print("El resultado es ",Ndeserie,":")
+   print(n1)
+else:
+   print("La sucecion es:")
+   while count < Ndeserie:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
